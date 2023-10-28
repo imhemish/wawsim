3aecb1cb561561c49ce524014b12d4533731ade5
Hemish <hemish04082005@gmail.com>
2023-10-28 18:46:19 +0530

    adjusted pixel ratio

--- lib/util.dart
+++ lib/util.dart
@@ -2,7 +2,8 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 num getAdjustedPixelRatio(context) {
-  if (defaultTargetPlatform == TargetPlatform.linux) {
+  if (defaultTargetPlatform == TargetPlatform.linux ||
+      defaultTargetPlatform == TargetPlatform.windows) {
     return MediaQuery.of(context).devicePixelRatio * 3;
   } else {
     return MediaQuery.of(context).devicePixelRatio;
