b7c39943d3ec5d2d93344bf50fb087bd1e0c62ad
Hemish <hemish04082005@gmail.com>
2023-12-27 16:29:47 +0530

    add animation to sunrise and sunset

--- lib/widgets/sun_details.dart
+++ lib/widgets/sun_details.dart
@@ -1,11 +1,34 @@
 import 'package:flutter/material.dart';
 import './darker_container.dart';
 
-class SunDetails extends StatelessWidget {
+class SunDetails extends StatefulWidget {
   const SunDetails(this.sunset, this.sunrise);
   final String sunset;
   final String sunrise;
 
+  @override
+  State<SunDetails> createState() => _SunDetailsState();
+}
+
+class _SunDetailsState extends State<SunDetails> with SingleTickerProviderStateMixin{
+  late AnimationController _animationController;
+  final _colorTween = ColorTween(begin: Colors.transparent.withAlpha(25), end: Colors.transparent.withAlpha(200));
+  late Animation<Color?> _colorTweenAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1))..forward()..repeat(reverse: true);
+    _colorTweenAnimation = _colorTween.animate(_animationController);
+
+  }
+
+  @override
+void dispose() {
+  _animationController.dispose();
+  super.dispose();
+}
   @override
   Widget build(BuildContext context) {
     var height = MediaQuery.sizeOf(context).height;
@@ -14,62 +37,65 @@ class SunDetails extends StatelessWidget {
     return CustomDarkerContainer(
         height / 4,
         null,
-        Container(
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                image: DecorationImage(
-                    image: AssetImage("assets/moonrise.jpg"),
-                    fit: BoxFit.cover,
-                    colorFilter: ColorFilter.mode(
-                        Colors.transparent.withAlpha(100), BlendMode.dstATop))),
-            child: Padding(
-              padding: EdgeInsets.symmetric(
-                  horizontal: width / 18, vertical: height / 28),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Icon(
-                          Icons.sunny_snowing,
-                          color: Colors.white,
-                        ),
-                        Text.rich(
-                          TextSpan(children: [
-                            TextSpan(
-                                text: "Sunset\n",
-                                style: TextStyle(
-                                    color: Color.fromARGB(255, 174, 184, 210))),
-                            TextSpan(
-                                text: sunset,
-                                style: TextStyle(color: Colors.white))
-                          ]),
-                          textAlign: TextAlign.center,
-                        )
-                      ]),
-                  Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Icon(
-                          Icons.sunny,
-                          color: Colors.white,
-                        ),
-                        Text.rich(
-                          TextSpan(children: [
-                            TextSpan(
-                                text: "Sunrise\n",
-                                style: TextStyle(
-                                    color: Color.fromARGB(255, 174, 184, 210))),
-                            TextSpan(
-                                text: sunrise,
-                                style: TextStyle(color: Colors.white))
-                          ]),
-                          textAlign: TextAlign.center,
-                        )
-                      ]),
-                ],
-              ),
-            )));
+        AnimatedBuilder(
+          animation: _animationController,
+          builder: (context, child) => Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  image: DecorationImage(
+                      image: AssetImage("assets/moonrise.jpg"),
+                      fit: BoxFit.cover,
+                      colorFilter: ColorFilter.mode(
+                          _colorTweenAnimation.value!, BlendMode.dstATop))),
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: width / 18, vertical: height / 28),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Icon(
+                            Icons.sunny_snowing,
+                            color: Colors.white,
+                          ),
+                          Text.rich(
+                            TextSpan(children: [
+                              TextSpan(
+                                  text: "Sunset\n",
+                                  style: TextStyle(
+                                      color: Color.fromARGB(255, 174, 184, 210))),
+                              TextSpan(
+                                  text: widget.sunset,
+                                  style: TextStyle(color: Colors.white))
+                            ]),
+                            textAlign: TextAlign.center,
+                          )
+                        ]),
+                    Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Icon(
+                            Icons.sunny,
+                            color: Colors.white,
+                          ),
+                          Text.rich(
+                            TextSpan(children: [
+                              TextSpan(
+                                  text: "Sunrise\n",
+                                  style: TextStyle(
+                                      color: Color.fromARGB(255, 174, 184, 210))),
+                              TextSpan(
+                                  text: widget.sunrise,
+                                  style: TextStyle(color: Colors.white))
+                            ]),
+                            textAlign: TextAlign.center,
+                          )
+                        ]),
+                  ],
+                ),
+              )),
+        ));
   }
 }
